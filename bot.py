b0b43cdac0926a6b863423b8c41cce5af5bdd722
kitanin90 <kitanin90@mail.ru>
2019-07-21 23:44:19 +0500

    test new function

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    # bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.text)
 
 
 if __name__ == '__main__':
